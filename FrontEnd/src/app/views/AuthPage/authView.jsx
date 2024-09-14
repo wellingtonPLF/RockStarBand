cf8ce52bfdd97e8232fc692b449758ec630513f0
wellingtonPLF <wellplf@gmail.com>
2024-09-14 15:38:49 -0300

    Add auth interface

--- FrontEnd/src/app/views/AuthPage/authView.jsx
+++ FrontEnd/src/app/views/AuthPage/authView.jsx
@@ -1,4 +1,6 @@
 import styles from "./authStyle.module.css"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faApple, faGoogle, faMicrosoft, faWindows, faXTwitter} from "@fortawesome/free-brands-svg-icons";
 
 const AuthView = (props) => {
 
@@ -6,12 +8,52 @@ const AuthView = (props) => {
       <>
           <div id={styles.authPage}>
             <div>
-              <label>username</label>
-              <input />
+              <div>
+                <div id={styles.topAuth}>
+                  <h1>Welcome to SuperVisionary</h1>
+                  <div>Please enter your authentication</div>
+                </div>
+                <div id={styles.auth}>
+                  <div>
+                    <label>username</label>
+                    <input />
+                  </div>
+                  <div>
+                    <label>password</label>
+                    <input />
+                  </div>
+                </div>
+                <div id={styles.bottomAuth}>
+                  <div>
+                    <input type="checkbox"></input>
+                    <label>Remember</label>
+                  </div>
+                  <div>Forgot Password</div>
+                </div>
+                <button id={styles.signIn}>Sing In</button>
+
+                <div id={styles.signUp}>New to SuperVisionary? <a>Create an account</a></div>
+              </div>
             </div>
-            <div>
-              <label>password</label>
-              <input />
+            <div id={styles.oAuth}>
+              <div>
+                <button>
+                  <FontAwesomeIcon icon={faGoogle} />
+                  Continue with Google
+                </button>
+                <button>
+                  <FontAwesomeIcon icon={faWindows} />
+                  Continue with Miscrosoft
+                </button>
+                <button>
+                  <FontAwesomeIcon icon={faApple} />
+                  Continue with Apple
+                </button>
+                <button disabled>
+                  <FontAwesomeIcon icon={faXTwitter} />
+                  Continue with Twitter
+                </button>
+              </div>
             </div>
           </div>
       </>
